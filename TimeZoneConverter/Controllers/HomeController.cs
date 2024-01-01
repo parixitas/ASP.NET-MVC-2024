8188c23fc17dfecfefb881cdebba583113e8f15d
ParixitaSakhiya <sakhiya.parixita@gmail.com>
2024-01-01 15:34:16 +0530

    latest code

---
+++ TimeZoneConverter/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeZoneConverter.Models;
+
+namespace TimeZoneConverter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new TimeZoneConverterModel
+            {
+                SelectedDateTime = DateTime.UtcNow,
+                SelectedTimeZone = "UTC"
+            };
+            ViewBag.msg = "";
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Convert(TimeZoneConverterModel model)
+        {
+            ViewBag.msg = "test";
+            if (ModelState.IsValid)
+            {
+                TimeZoneInfo sourceTimeZone = TimeZoneInfo.Utc;
+                TimeZoneInfo targetTimeZone = TimeZoneInfo.FindSystemTimeZoneById(model.SelectedTimeZone);
+
+                model.ConvertedDateTime = TimeZoneInfo.ConvertTime(model.SelectedDateTime, sourceTimeZone, targetTimeZone);
+            }
+
+            return View("Index", model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TimeZoneConverter/Models/TimeZoneConverterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeZoneConverter.Models
+{
+    public class TimeZoneConverterModel
+    {
+        public DateTime SelectedDateTime { get; set; }
+        public string SelectedTimeZone { get; set; }
+        public DateTime ConvertedDateTime { get; set; }
+    }
+}
